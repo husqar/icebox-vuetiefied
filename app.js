1d24f44aaaf88d2776eb4e199e3a0152dc908db3
Johannes Gosch <j.gosch@husqar.net>
2018-06-24 05:41:59 +0200

    weiter geht es

--- app.js
+++ app.js
@@ -1,76 +1,106 @@
 const baseUrl = "http://icebox.nobreakspace.org:8081/";    
-    
-    const Overview = {
-        template: '#overview-template'
-    }
+const eventHub = new Vue();
 
-    const Keyboard = {
-        template: '#keyboard-template',
-        data: () =>({
-            input: '',
-            letters: ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-        }),
-        methods:{
-            pressed(event){
-                this.input += event.target.firstChild.data.toLowerCase();
-            },
-            deleteInput(){
-                this.input = '';
-            }
+// Distribute to components using global mixin
+Vue.mixin({
+    data: function () {
+        return {
+            eventHub: eventHub
         }
     }
+})
 
-    const Drinks =  {
-        template: '#drinks-template',
-        data: () => ({
-            drinks: []
-        }),
-        mounted() {
-            this.getDrinks();
+const Overview = {
+    template: '#overview-template'
+}
+
+const Keyboard = {
+    template: '#keyboard-template',
+    data: () =>({
+        input: '',
+        letters: ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    }),
+    methods:{
+        pressed(event){
+            this.input += event.target.firstChild.data.toLowerCase();
+            this.eventHub.$emit('update',this.input);
         },
-        methods: {
-			getDrinks() {
-				axios.get(baseUrl + `drinks`).then(response => {
-                    this.drinks = response.data
-                    this.drinks.map(drink => drink.image = 'img/bottle.png');
-					console.log(this.drinks);
-				}).catch(error => {
-					console.log(error);
-				})
-			}
+        deleteInput(){
+            this.input = '';
+            this.eventHub.$emit('update',this.input);
         }
-    };    
+    }
+}
 
-    const Consumers = {
-        template: '#consumers-template',
-        data: () => ({
-            consumers: []
-        }),
-        mounted(){
-            this.getConsumers();
-        },
+const Drinks =  {
+    template: '#drinks-template',
+    data: () => ({
+        drinks: []
+    }),
+    mounted() {
+        this.getDrinks();
+    },
+    methods: {
+        getDrinks() {
+            axios.get(baseUrl + `drinks`).then(response => {
+                this.drinks = response.data
+                this.drinks.map(drink => drink.image = 'img/bottle.png');
+                console.log(this.drinks);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    }
+};
 
-        methods: {
-            getConsumers(){
-                axios.get(baseUrl + `consumers`).then(response => {
-                    this.consumers = response.data
-                    console.log(this.consumers);
-                }).catch(error => {
-                    console.log(error);
-                })
+const Orderpanel = {
+    template: '#orderpanel-template'
+}
+
+const Consumers = {
+    template: '#consumers-template',
+    data: () => ({
+        consumers: [],
+        filterString: ''
+    }),
+    computed:{
+        filteredConsumers(){
+            function startsWith(wordToCompare) {
+                return function(string) {
+                    return string.username.indexOf(wordToCompare) === 0;
+                }
             }
+            console.log(this.filterString);
+            return this.consumers.filter(
+                startsWith(this.filterString)
+            );
         }
-    };
+    },
+    mounted(){
+        this.getConsumers();
+        this.eventHub.$on('update', data => {
+                this.filterString = data;
+            });
+    },
+    methods: {
+        getConsumers(){
+            axios.get(baseUrl + `consumers`).then(response => {
+                this.consumers = response.data
+                console.log(this.consumers);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    }
+};
+
+Vue.component('orderpanel', Orderpanel);
+Vue.component('consumers', Consumers);
+Vue.component('drinks', Drinks);
+Vue.component('keyboard', Keyboard);
+Vue.component('overview', Overview);
 
 
-    Vue.component('consumers', Consumers);
-    Vue.component('drinks', Drinks);
-    Vue.component('keyboard', Keyboard);
-    Vue.component('overview', Overview);
-    console.log(Vue);
-    
-    
-    const vue = new Vue();
-    var app = vue.$mount('#app');
+const vue = new Vue();
+var app = vue.$mount('#app');
 
-	
